0ee5d6bb94869801763caa1e172f003ec8f8e2f8
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-12-02 12:36:58 +0100

    day 2 part 2

--- 2023/02/solution.py
+++ 2023/02/solution.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+import math
 import re
+from collections import defaultdict
 
 max_vals = {
     "red": 12,
@@ -19,5 +21,15 @@ def valid_game_id(record):
     return int(game_id)
 
 
+def game_set_pow(record):
+    cnt = defaultdict(lambda: 0)
+    for count in re.split(",|;", record.split(": ")[1]):
+        amount, color = count.split()
+        cnt[color] = max(cnt[color], int(amount))
+    return math.prod(cnt.values())
+
+
 with open("input.txt") as f:
-    print(sum(valid_game_id(record) for record in l))
+    print(sum(valid_game_id(record) for record in f))
+    f.seek(0)
+    print(sum(game_set_pow(record) for record in f))
