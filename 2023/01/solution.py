0b392a71e127a366644e799762af90a4095ba6d6
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-12-02 11:22:31 +0100

    day 1 part 2

--- 2023/01/solution.py
+++ 2023/01/solution.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+digits = ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
 
 
 def part_1(input):
@@ -15,8 +16,30 @@ def part_1(input):
     return int(d1 + d2)
 
 
-sum = 0
+def part_2(input):
+    def d_1():
+        for i in range(len(input)):
+            if input[i].isdigit():
+                return input[i]
+            for v, d in enumerate(digits, 1):
+                if input[i:].startswith(d):
+                    return str(v)
+
+    def d_2():
+        for i in reversed(range(len(input))):
+            if input[i].isdigit():
+                return input[i]
+            for v, d in enumerate(digits, 1):
+                if input[:i].endswith(d):
+                    return str(v)
+
+    return int(d_1() + d_2())
+
+
+sum, sum_2 = 0, 0
 with open("input.txt") as f:
     for line in f:
-        sum += get_num(line)
+        sum += part_1(line)
+        sum_2 += part_2(line)
 print(sum)
+print(sum_2)
