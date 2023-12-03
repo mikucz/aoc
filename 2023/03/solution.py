599470c98205b761fb52b932aecf583ed8efa788
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-12-03 20:09:09 +0100

    day 3 part 2

--- 2023/03/solution.py
+++ 2023/03/solution.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import math
 import re
 
 with open("input.txt") as f:
@@ -19,6 +20,7 @@ def is_adjecent(y, x_start, x_end):
     return False
 
 
+# part 1
 print(
     sum(
         [
@@ -33,3 +35,28 @@ print(
         ]
     )
 )
+
+gears_coordinates = sum(
+    [[(m.start(), y) for m in re.finditer("[*]", line)] for y, line in enumerate(data)],
+    [],
+)
+numbers_coordinates = [
+    [(m.start(), m.end(), int(m[0])) for m in re.finditer("[0-9]+", line)]
+    for line in data
+]
+
+
+def gear_adjecent_numbers(gear):
+    y_start, y_end = max(0, gear[1] - 1), min(len(numbers_coordinates), gear[1] + 2)
+    adjecent_nums = []
+    for y in range(y_start, y_end):
+        for num in numbers_coordinates[y]:
+            if num[0] - 1 <= gear[0] <= num[1]:
+                adjecent_nums.append(num[2])
+    if len(adjecent_nums) < 2:
+        return 0
+    return math.prod(adjecent_nums)
+
+
+# part 2
+print(sum([gear_adjecent_numbers(gear) for gear in gears_coordinates]))
