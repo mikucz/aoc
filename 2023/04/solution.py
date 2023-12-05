6d95de8cc5d0790b81974014796f0b43d29a90a5
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-12-05 20:07:58 +0100

    day 4 part 2

--- 2023/04/solution.py
+++ 2023/04/solution.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
+from collections import defaultdict
+
+
 def parse_card(input):
-    nums = line.split(":")[1].split("|")
+    card, nums = input.split(":")
+    nums = nums.split("|")
+    card = int(card.split()[1])
     winning = set(map(int, nums[0].split()))
     owned = set(map(int, nums[1].split()))
-    return winning, owned
+    return card, winning, owned
 
 
 def count_points(winning, owned):
@@ -13,9 +18,28 @@ def count_points(winning, owned):
     return 0
 
 
-sum = 0
+def count_cards(card, winning, owned, cards_cnt):
+    matches = len(winning.intersection(owned))
+    cards_cnt[card] += 1
+    if not matches:
+        return
+    for i in range(1, matches + 1):
+        if cards_cnt[card] > 1:
+            cards_cnt[card + i] += cards_cnt[card]
+        else:
+            cards_cnt[card + i] += 1
+
+
+sum_1, sum_2 = 0, 0
 with open("input.txt") as f:
     for line in f:
-        sum += count_points(*parse_card(line))
+        sum_1 += count_points(*parse_card(line)[1:])
 
-print(sum)
+with open("input.txt") as f:
+    cards_cnt = defaultdict(int)
+    for line in f:
+        count_cards(*parse_card(line), cards_cnt)
+    sum_2 = sum(cards_cnt.values())
+
+print(sum_1)
+print(sum_2)
