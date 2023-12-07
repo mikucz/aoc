cee8a5cacbfbca8b6f40ed404a689732becf9e6b
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-12-07 19:26:26 +0100

    day 7 part 2

--- 2023/07/solution.py
+++ 2023/07/solution.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import re
 
-# A, K, Q, J, T -> H, G, F, E, D
-translate_table = str.maketrans("AKQJT", "HGFED")
+# A, K, Q, J, T -> M, L, K, J, I
+translate_table = str.maketrans("AKQJT", "MLKJI")
 # in order for regex to work string must be in lexicographic order and
-# card with highest value must be first (thats why AKQJT HGFED remap)
+# card with highest value must be first (thats why AKQJT MLKJI remap)
+# leave J in same place for part 2
 types = [
     r"(.)\1{4}",  # five of kind
     r"(.)\1{3}",  # four of kind
@@ -40,11 +41,31 @@ def points(hand):
     return 0
 
 
+# part 1
 print(
     sum(
         bid * i
         for i, (_, bid) in enumerate(
             sorted(data, key=lambda d: (points(d[0]), d[0])), start=1
+        )
+    )
+)
+
+# part 2
+# find best J variant by replacing it with each card and checking points
+# for ties change J to 1
+print(
+    sum(
+        i * bid
+        for i, (_, bid) in enumerate(
+            sorted(
+                data,
+                key=lambda d: (
+                    max(points(d[0].replace("J", c)) for c in d[0]),
+                    d[0].replace("J", "1"),
+                ),
+            ),
+            1,
         )
     )
 )
