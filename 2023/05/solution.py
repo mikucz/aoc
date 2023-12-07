4b86297bca0d1b3d6167b1fe223b4713f2854c10
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-12-07 15:16:41 +0100

    day 5 part 2

--- 2023/05/solution.py
+++ 2023/05/solution.py
@@ -15,4 +15,23 @@ for seed in seeds:
                 seed = dst + seed - src
                 break
     location.append(seed)
-print(min(location))
+# print(min(location))
+
+
+# part 2
+seed_ranges = zip(seeds[::2], seeds[1::2])
+
+remain = set(seed_ranges)
+done = set()
+for map in maps:
+    remain, done = done.union(remain), set()
+    for dst, src, rng in map:
+        for s, r in [(s, r) for s, r, in remain if s + r > src and s < src + rng]:
+            remain.remove((s, r))
+            if s < src:
+                remain.add((s, src - s))
+            if s + r > src + rng:
+                remain.add((src + rng, s + r - src - rng))
+            done.add((dst + max(s - src, 0), min(s + r, src + rng) - max(src, s)))
+
+print(min(remain | done)[0])
