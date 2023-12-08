c1982ec9d04e9c35201a12dacf6db1fd4d5f94e1
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-12-08 20:25:47 +0100

    day 8 part 2

--- 2023/08/solution.py
+++ 2023/08/solution.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 from itertools import cycle
+from math import lcm
 from re import findall
 
 with open("input.txt") as f:
     moves = list(map(int, f.readline().translate(str.maketrans("LR", "01")).strip()))
     data = {
         el: (left, right)
-        for el, left, right in (findall("[A-Z]+", line) for line in f if line.strip())
+        for el, left, right in (
+            findall("[0-9A-Z]+", line) for line in f if line.strip()
+        )
     }
 
 
 def steps(start, end):
     choice = data[start]
     for i, move in enumerate(cycle(moves), 1):
-        if choice[move] == end:
+        if choice[move].endswith(end):
             return i
         choice = data[choice[move]]
 
-print(steps("AAA", "ZZZ"))
+
+# part 1
+# print(steps("AAA", "ZZZ"))
+
+# part 2
+print(lcm(*(steps(node, "Z") for node in data.keys() if node.endswith("A"))))
