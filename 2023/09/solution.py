2bb0a44087be5dcc3ebf66988674d8cc5e657952
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-12-13 18:38:01 +0100

    day 9 part 2

--- 2023/09/solution.py
+++ 2023/09/solution.py
@@ -14,4 +14,18 @@ def extrapolate(data):
 
     return data[-1] + rec(data)
 
+
+def extrapolate_backwards(data):
+    def rec(data):
+        sequence = [b - a for a, b in pairwise(data)]
+        if any(sequence):
+            return sequence[0] - rec(sequence)
+        return 0
+
+    return data[0] - rec(data)
+
+
+# part 1
 print(sum(extrapolate(d) for d in data))
+# part 2
+print(sum(extrapolate_backwards(d) for d in data))
