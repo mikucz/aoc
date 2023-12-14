f41c6db7d20e1a28716d147a02bfc72dda31a1b4
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-12-14 12:33:08 +0100

    day 10 part 2

--- 2023/10/solution.py
+++ 2023/10/solution.py
@@ -19,13 +19,27 @@ move_x_map = {0: -1, 1: 0, 2: 1, 3: 0}
 move_y_map = {0: 0, 1: -1, 2: 0, 3: 1}
 
 
-p, cnt, d, d_start = start, 0, 0, 0
+p, cnt, d, d_start, loop = start, 0, 0, 0, set()
 while p != start or cnt == 0:
     "if beginning direction not found -> start with new direction"
     if d == -1:
-        p, cnt, d, d_start = start, 0, d_start + 1, d_start + 1
+        p, cnt, d, d_start, loop = start, 0, d_start + 1, d_start + 1, set()
     p = (p[0] + move_x_map[d], p[1] + move_y_map[d])
+    loop.add(p)
+    s_tile = direction[d][d_start]  # replacement tile for S
     d = direction[d].find(data[p[1]][p[0]])
     cnt += 1
 
 print(cnt // 2)
+
+
+enclosed, enclosed_tiles = False, 0
+for j, line in enumerate(data):
+    for i, t in enumerate(line.replace("S", s_tile)):
+        if (i, j) in loop:
+            e = enclosed
+            enclosed = enclosed != (t in "F7|")
+        elif enclosed:
+            enclosed_tiles += 1
+
+print(enclosed_tiles)
