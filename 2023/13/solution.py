579e8683ca32471c945f38fd4c389652dda53aef
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-12-16 14:29:00 +0100

    day 13 part 1

---
+++ 2023/13/solution.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import numpy as np
+
+with open("input.txt") as f:
+    data = [
+        np.array(
+            [[c == "#" for c in line] for line in mirror.split("\n")],
+            dtype=int,
+        )
+        for mirror in f.read().strip().split("\n\n")
+    ]
+
+
+def mirror_index(m):
+    for i in range(1, m.shape[0]):
+        a, b = m[max(0, 2 * i - m.shape[0]) : i], m[i : min(2 * i, m.shape[0])]
+        if not np.count_nonzero(a != b[::-1]):
+            return i
+    return 0
+
+
+print(
+    sum(
+        [
+            mirror_index(mirror) * 100 or mirror_index(mirror.transpose())
+            for mirror in data
+        ]
+    )
+)
