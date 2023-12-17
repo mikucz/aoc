49acaaab18d4072645fdd3ed5156ebfd105a77a6
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-12-17 11:29:30 +0100

    day 14 part 2

--- 2023/14/solution.py
+++ 2023/14/solution.py
@@ -3,14 +3,52 @@ import re
 from itertools import chain
 
 with open("input.txt") as f:
-    data = [line.strip() for line in f]
+    data = tuple((line.strip() for line in f))
 
-transposed = ["".join(l) for l in zip(*data)]
 
-for i in range(len(transposed)):
-    transposed[i] = "".join(
-        chain(*[reversed(sorted(chunk)) for chunk in re.split(r"(#+)", transposed[i])])
+def rotate(m, i=1):
+    return tuple("".join(reversed(l)) for l in zip(*m))
+
+
+def tilt(m):
+    return tuple(
+        "".join(chain(*[sorted(chunk) for chunk in re.split(r"(#+)", l)])) for l in m
     )
 
-counts = [l.count("O") for l in zip(*transposed)]
-print(sum([c * i for i, c in enumerate(reversed(counts), 1)]))
+
+def load(m):
+    return sum([c * i for i, c in enumerate(reversed([l.count("O") for l in m]), 1)])
+
+
+# part 1
+# rotate 1, tilt east, rotate 3 more to return into orig pos, calc load
+t = tilt(rotate(data))
+for _ in range(3):
+    t = rotate(t)
+print(load(t))
+
+
+# part 2
+cycles = 1000000000
+cache = dict()
+head = -1
+for cycle in range(cycles):
+    if (cached_index := cache.get(data)) is not None:
+        head = cached_index
+        repeat_count = cycle - head
+        break
+    cache[data] = cycle
+    for _ in range(4):
+        data = tilt(rotate(data))
+
+print(
+    load(
+        next(
+            (
+                m
+                for m, n in cache.items()
+                if n - head == (cycles - head) % (cycle - head)
+            )
+        )
+    )
+)
