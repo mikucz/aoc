85e0c12ab2a2dff4beba3099a7eb8abfa4f495a6
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-12-17 13:14:54 +0100

    day 15 part 2

--- 2023/15/solution.py
+++ 2023/15/solution.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from collections import OrderedDict, defaultdict
+from re import split
 
 with open("input.txt") as f:
     data = [h.strip() for h in f.read().split(",")]
@@ -10,4 +12,29 @@ def hash_alg(s):
         i = (i + ord(c)) * 17 % 256
     return i
 
+
+# part 1
 print(sum((hash_alg(s) for s in data)))
+
+# part 2
+boxes = defaultdict(OrderedDict)
+for d in data:
+    label, operation, count = split("([-=])", d)
+    box_id = hash_alg(label)
+    if operation == "=":
+        boxes[box_id][label] = int(count)
+    elif operation == "-":
+        boxes[box_id].pop(label, None)
+
+
+print(
+    sum(
+        (
+            sum(
+                (box + 1) * i * focal_len
+                for i, focal_len in enumerate(lenses.values(), 1)
+            )
+            for box, lenses in boxes.items()
+        )
+    )
+)
